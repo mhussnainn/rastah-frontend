159c9a5c718bf20b47d7c7ed7ae136435f7e4550
mhussnainn <hussnain.anwar19@gmail.com>
2025-05-25 18:22:01 +0500

    Increased responsiveness

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
     domains: [ 'victorious-prize-eeb50f2b32.media.strapiapp.com' , 'victorious-prize-eeb50f2b32.strapiapp.com'],
   },
 };
-module.exports = {
-  experimental: {
-    modern: true,
-  },
-};
+
 
 export default nextConfig;
